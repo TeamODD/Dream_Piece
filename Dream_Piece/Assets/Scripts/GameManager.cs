89aef6c6c1be897282ad88feedd6b955c5943fca
hipop1109-commits <hipop1109@gmail.com>
2025-05-08 17:34:34 +0900

    [FEAT] GameManager
    게임매니저

---
+++ Dream_Piece/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    //public Player player;
+    private int DreamPiece = 0;
+    [SerializeField]
+    private int DreamPiece1 = 10;
+    [SerializeField]
+    private int DreamPiece2 = 10;
+    [SerializeField]
+    private int DreamPiece3 = 10;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        DreamPiece = 0;
+    }
+
+    public void AddDreamPiece()
+    {
+        DreamPiece++;
+    }
+
+    public void StageClear()
+    {
+        if(SceneManager.GetActiveScene().name == "Stage1Scene" && DreamPiece == DreamPiece1)
+        {
+            SceneManager.LoadScene("Stage2Scene");
+        }
+        else if (SceneManager.GetActiveScene().name == "Stage2Scene" && DreamPiece == DreamPiece2)
+        {
+            SceneManager.LoadScene("Stage3Scene");
+        }
+        else if (SceneManager.GetActiveScene().name == "Stage3Scene" && DreamPiece == DreamPiece3)
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+        else
+        {
+            
+            return;
+        }
+    }
+}
